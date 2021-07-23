745efdc066f89430d7fb1b5513167d1521240523
Stephen Kurshner <skurshner@gmail.com>
2021-07-23 15:18:28 -0700

    added event listener to mouseenter event for each div

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // assign grid container reference
 const grid = document.getElementById('container');
 
-// assign div reference
-const divs = document.getElementsByClassName('square')
-
 // create divs to enter into grid
 function createDivs (n) {
     for (i = 0; i < n*n; i++) {
@@ -11,8 +8,16 @@ function createDivs (n) {
         div.classList = "square";
         grid.appendChild(div);
         grid.style.gridTemplateColumns = 'repeat('+ n +', 1fr)'
-
     }
 }
-
 createDivs(100);
+
+// assign class to div on hover
+const divs = document.querySelectorAll('.square');
+    divs.forEach((div) => {
+        div.addEventListener('mouseenter', () => {
+            div.classList = "filled";
+        })
+    }) 
+
+;
