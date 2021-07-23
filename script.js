290008804d2ccc0f9e582eca0eb080b99f59b02a
Stephen Kurshner <skurshner@gmail.com>
2021-07-23 14:31:28 -0700

    createDivs function now takes in number and creates proper grid

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // assign grid container reference
 const grid = document.getElementById('container');
 
+// assign div reference
+const divs = document.getElementsByClassName('square')
+
 // create divs to enter into grid
 function createDivs (n) {
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n*n; i++) {
         let div = document.createElement('div');
-        div.classList = "square"
+        div.classList = "square";
         grid.appendChild(div);
+        grid.style.gridTemplateColumns = 'repeat('+ n +', 1fr)'
 
     }
 }
-    
-createDivs(16)
+
+createDivs(100);
