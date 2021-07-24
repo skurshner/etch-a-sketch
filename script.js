3b76ddd974724a5d245aaa28bda8c8d4aa50ae64
Stephen Kurshner <skurshner@gmail.com>
2021-07-23 18:44:49 -0700

    added function to clear board and prompt for new amount of squares

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 // assign grid container reference
 const grid = document.getElementById('container');
 
+promptNumberOfSquares();
+
+// prompt user for number of square
+function promptNumberOfSquares () {
+    let numberOfSquares = prompt('How many squares per side?')
+    if (numberOfSquares > 100) numberOfSquares = 100;   
+    createSquares(numberOfSquares);
+}
+
 // create divs to enter into grid
-function createDivs (n) {
+function createSquares (n) {
+    removeSquares();
     for (i = 0; i < n*n; i++) {
         let div = document.createElement('div');
         div.classList = "square";
         grid.appendChild(div);
         grid.style.gridTemplateColumns = 'repeat('+ n +', 1fr)'
     }
+    assignClass();
 }
-createDivs(100);
 
 // assign class to div on hover
-const divs = document.querySelectorAll('.square');
+function assignClass() {
+    const divs = document.querySelectorAll('.square');
     divs.forEach((div) => {
         div.addEventListener('mouseenter', () => {
-            div.classList = "filled";
+        div.classList = "filled";
         })
-    }) 
+    });
+}
 
-;
+function removeSquares () {
+    const divsEmpty = document.querySelectorAll('.square');
+    for (i = 0; i < divsEmpty.length; i++) {
+        divsEmpty[i].parentNode.removeChild(divsEmpty[i]);
+    }
+    const divsFilled = document.querySelectorAll('.filled');
+    for (i = 0; i < divsFilled.length; i++) {
+        divsFilled[i].parentNode.removeChild(divsFilled[i]);
+    }
+   
+}
+
+// clear squares with button and prompt user for new square amount
+const button = document.getElementById('clear');
+button.addEventListener('click', () => {
+    removeSquares();
+    promptNumberOfSquares();
+})
