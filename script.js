97c864f18b7b4bc5b31d4335e169509f7ec28ef6
Stephen Kurshner <skurshner@gmail.com>
2021-07-24 09:37:31 -0700

    cleaned up code and reduced number of lines

--- script.js
+++ script.js
@@ -1,51 +1,65 @@
 // assign grid container reference
 const grid = document.getElementById('container');
 
+// target clear button
+const button = document.getElementById('clear');
+    // when button clicked, run function to collect number of squares
+    button.addEventListener('click', promptNumberOfSquares)
+
+// prompt user for number of squares on page load
 promptNumberOfSquares();
 
-// prompt user for number of square
+// display a prompt to collect number of squares from user
 function promptNumberOfSquares () {
+    // set variable for number of squares based on prompt input
     let numberOfSquares = prompt('How many squares per side?')
-    if (numberOfSquares > 100) numberOfSquares = 100;   
+    // limit number of squares to 100
+    if (numberOfSquares > 100) numberOfSquares = 100;
+    // call function to create squares with amount entered by user   
     createSquares(numberOfSquares);
 }
 
-// create divs to enter into grid
+// create square divs to enter into grid
 function createSquares (n) {
+    // remove any squares that currently exist
     removeSquares();
+    // loop number entered by user times itself
     for (i = 0; i < n*n; i++) {
-        let div = document.createElement('div');
-        div.classList = "square";
-        grid.appendChild(div);
-        grid.style.gridTemplateColumns = 'repeat('+ n +', 1fr)'
+        // add a new square div in the DOM
+        let square = document.createElement('div');
+        // add class "square" to newly created div
+        square.classList = "square";
+        // add square div as child of grid
+        grid.appendChild(square);
+        // update grid attribute of parent grid columns to number entered by user
+        grid.style.gridTemplateColumns = 'repeat('+ n +', 1fr)';
+        grid.style.backgroundColor = 'black';
     }
+    // assign class to divs if they are hovered over
     assignClass();
 }
 
 // assign class to div on hover
 function assignClass() {
-    const divs = document.querySelectorAll('.square');
-    divs.forEach((div) => {
-        div.addEventListener('mouseenter', () => {
-        div.classList = "filled";
+    // select all elements with class "square" and create nodelist "squares"
+    const squares = document.querySelectorAll('.square');
+    // select each square within nodelist "squares"
+    squares.forEach((square) => {
+        // add event listener for mouse hover over "square"
+        square.addEventListener('mouseenter', () => {
+        // add class "filed" to "square" on mouse hover
+        square.classList.add('filled');
         })
     });
 }
 
+// remove any squares from the DOM
 function removeSquares () {
-    const empty = document.querySelectorAll('.square');
-    for (i = 0; i < empty.length; i++) {
-        empty[i].parentNode.removeChild(empty[i]);
-    }
-    const filled = document.querySelectorAll('.filled');
-    for (i = 0; i < filled.length; i++) {
-        filled[i].parentNode.removeChild(filled[i]);
+    // create nodelist "squares" from all divs with class "square"
+    const squares = document.querySelectorAll('.square');
+    // loop through each item in nodelist "squares"
+    for (i = 0; i < squares.length; i++) {
+        // remove all existing squares from the DOM
+        squares[i].parentNode.removeChild(squares[i]);
     }
 }
-
-// clear squares with button and prompt user for new square amount
-const button = document.getElementById('clear');
-button.addEventListener('click', () => {
-    removeSquares();
-    promptNumberOfSquares();
-})
